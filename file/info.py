90a13d81d55ed8f54054fa524a813b8d858d2b14
Kaz Malhotra <kazmal#protonmail.com>
2020-01-15 13:02:45 -0800

    add personal info script

--- file/info.py
+++ file/info.py
@@ -1,27 +1,15 @@
 import sys
 import os
-cwd = os.getcwd()
-path = "files"
-filename = 'files/info.csv'
+filename = input("What would you like your file to be called?   ")
 writefile = open(filename, "w+")
-
-try:
-    os.mkdir(path)
-except OSError:
-    print ("The directory %s already exists" % path)
-else:
-    print ("Successfully created the directory %s " % path)
-
 
 name = input("What is your name?    ")
 age = input("how old are you?   ")
 info = [name, age]
 
-
-with open(filename, 'a') as filehandle:
-    for listitem in info:
-        filehandle.write('%s\n' % listitem)
-
+writefile.write(str(name))
+writefile.write(', ')
+writefile.write(str(age))
 
 
 writefile.close()
